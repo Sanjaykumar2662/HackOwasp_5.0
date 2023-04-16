3d55454fcd6fe83d6b5cf148d541f40062d427fc
LAPTOP-36C9AG9R\Jayanthan <jayanthandrj@gmail.com>
2023-04-16 11:53:10 +0530

    .

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import UploadImages from "./components/image-upload.component";
+const sdk = require('api')('@verbwire/v1.0#4psk2mplfwliyql');
 
+sdk.postNftStoreFile({accept: 'application/json'})
+  .then(({ data }) => console.log(data))
+  .catch(err => console.error(err));
 function App() {
   return (
     <div className="container">
       <h3>Smart Exam IDs</h3>
       <h4>Upload the Student Photo to IPFS</h4>
-
+      
       <div className="content">
         <UploadImages />
       </div>
